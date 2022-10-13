360157b1332880a4638257692741c628c7838ebc
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-13 13:52:10 -0700

    Second attempt at fixing 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
+int write(int filedes, const char *buf, unsigned int nbyte);
+
 int main(void)
 {
-	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
