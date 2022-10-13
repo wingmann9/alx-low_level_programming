ef92129da4b17b99f5688ae594b74a9c86382e80
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-13 14:00:29 -0700

    Fix the error in 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,8 @@
  *
  * Return: Always 1 (Success)
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
