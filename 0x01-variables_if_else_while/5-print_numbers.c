4e5ed43232314d1a85e0c816071a3ecf1b4d88fd
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-14 05:51:55 -0700

    Print single digit numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Displays all single digit numbers of base 10 starting from 0.
+ *
+ * Return: 0 (Success).
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
