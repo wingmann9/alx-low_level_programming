9ba74c3a174d4f017edb63e065a8e797cb6eeef4
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-18 16:26:13 -0700

    Fix error in file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,44 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - Prints the times table of the input,
- * @n: The value of the times table to be printed.
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
  */
 
-void print_times_table(int n)
+void print_to_98(int n)
 {
-	int num, mult, prod;
-
-	if (n >= 0 && n <= 15)
+	if (n >= 98)
 	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
 
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
-		}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
