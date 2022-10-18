348c62ca2aa7ec964bbd889c3f06d160e1827628
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-18 16:27:12 -0700

    Fix error in file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,44 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Lists all the natural numbers below 1024,
- *        that are multiples of 3 or 5.
- *
- * Return: Always 0.
+ * print_times_table - Prints the times table of the input,
+ * @n: The value of the times table to be printed.
  */
 
-int main(void)
+void print_times_table(int n)
 {
-	int i, sum = 0;
+	int num, mult, prod;
 
-	for (i = 0; i < 1024; i++)
+	if (n >= 0 && n <= 15)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	}
-
-	printf("%d\n", sum);
-
-	return (0);
 }
