75a5855d7be40b1da004fa71753f4a207c1e82f2
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-20 02:28:50 -0700

    Multiply given integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two given integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: Result of the multiplication.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
