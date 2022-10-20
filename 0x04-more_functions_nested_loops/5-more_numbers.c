a9374047614983eea3fb6c957714b77b3be4224e
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-20 04:47:47 -0700

    Print a range of numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the range of numbers 10 times.
+ */
+
+void more_numbers(void)
+{
+	int n, count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n')
+	}
+}
