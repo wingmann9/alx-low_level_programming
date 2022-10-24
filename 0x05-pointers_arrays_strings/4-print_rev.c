4bfd9c2baa908380fbf308d22bfe8a1d217f76ec
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-24 12:18:16 -0700

    Fix error in file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,17 +3,22 @@
 /**
  * print_rev - Prints a string in reverse.
  * @s: String to be printed.
+ * Return: void.
  */
 
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
 
-	while (s[index++])
+	while (*(s + len))
 		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	
+	len = len -1;
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
 
 	_putchar('\n');
 }
