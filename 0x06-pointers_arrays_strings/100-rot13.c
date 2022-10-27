d79a02d550d12428f7bebb7f8ccfce7db21d5081
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-26 19:19:04 -0700

    Encode a string into rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: String to be encoded.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	int e1 = 0, e2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[e1])
+	{
+		for (e2 = 0; e2 < 52; e2++)
+		{
+			if (str[e1] == alphabet[e2])
+			{
+				str[e1] = rot13key[e2];
+				break;
+			}
+		}
+
+		e1++;
+	}
+
+	return (str);
+}
