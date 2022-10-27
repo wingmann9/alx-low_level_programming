ef0565dcf64f619e9ffd545b7dff6b963ac4f990
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-10-26 19:01:20 -0700

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies an inputted number of bytes 
+ *            from string src into dest.
+ * @dest: Buffer storing the string copy.
+ * @src: Source string.
+ * @n: Maximum number of bytes to copied from src.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
