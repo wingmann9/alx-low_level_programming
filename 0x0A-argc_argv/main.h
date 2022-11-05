714dfe1889fde2225fb151c65684c8f86a5d4317
Ema-Eno Ukpong <emaaeno@gmail.com>
2022-11-04 17:01:35 -0700

    Header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Ema-Eno Simon
+ * Desc: Header file containing all functions prototype.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+
+#endif
