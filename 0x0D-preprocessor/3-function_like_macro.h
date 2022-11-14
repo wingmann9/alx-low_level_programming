8448139fe8791d0bc5cdde903d8c1d2f8d07f30e
wingmann <ema_eno@zoho.com>
2022-11-14 16:09:14 +0000

    Header file containing function-like macro ABS(x)

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Auth: wingmann
+ * Desc: Header file containing macro-like function
+ *	that compute the absolute value of int x.
+ * @x: Integer.
+ */
+
+#define ABS(x) abs(x)
+
+#endif
