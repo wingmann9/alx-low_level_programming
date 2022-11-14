d317d53aef62ab128336ce486b9653a5892bafed
wingmann <ema_eno@zoho.com>
2022-11-14 15:42:17 +0000

    Header file defining macro SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef MACRO_H
+#define MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Auth: wingmann
+ * Desc: Header file defining the macro named SIZE.
+ */
+
+#define SIZE 1024
+
+#endif
