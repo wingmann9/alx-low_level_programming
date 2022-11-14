fde6743d698983a10f498459b6da535a517662c0
wingmann <ema_eno@zoho.com>
2022-11-14 16:07:58 +0000

    A program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the file containing the program.
+ *
+ * Return: EXIIT_SUCCESS, otherwise EXIT_FAILURE.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	exit(EXIT_SUCCESS);
+}
