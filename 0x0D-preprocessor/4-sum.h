c1a901739296dbb1aaca199d03f22c289b5bbd10
wingmann <ema_eno@zoho.com>
2022-11-14 16:34:43 +0000

    Header file that compute the sum of x & y with macro SUM(x, y)

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Auth: wingmann
+ * Desc: Header file containing macro-like function
+ *	that compute the sum of int x and y.
+ * @x: First Integer.
+ * @y: Second Integer.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
