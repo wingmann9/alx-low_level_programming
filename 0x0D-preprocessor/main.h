a379ea5c92ddd859fd439a3390f6bcb7c406442b
wingmann <ema_eno@zoho.com>
2022-11-14 15:41:26 +0000

    Header file for 2-main.c

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Ema-Eno Simon
+ * Desc: Header file containing the standard library for 2-main.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
