9acf6276657c11476e0dde3892d471ba7a308f8e
wingmann <ema_eno@zoho.com>
2022-11-17 02:27:12 +0000

    Fix error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -9,6 +10,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
