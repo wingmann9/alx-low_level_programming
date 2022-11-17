5c5500d99cb587223bb914b0a6d6ff268b183a35
wingmann <ema_eno@zoho.com>
2022-11-17 02:31:25 +0000

    Fix error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,8 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL || action != NULL)
+	if (array == NULL || action == NULL)
+		return;
 
 	while (size-- > 0)
 	{
