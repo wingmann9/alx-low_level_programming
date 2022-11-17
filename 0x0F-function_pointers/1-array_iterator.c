30a8d7882fb9879f8822626f0b48e0ef4d6e4ddd
wingmann <ema_eno@zoho.com>
2022-11-17 01:46:03 +0000

    Execute a function given as a parameter on each array element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: Array to execute the function on.
+ * @size: Size of array.
+ * @action: Pointer to the function to be executed.
+ *
+ * Return: Void.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL || action != NULL)
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
