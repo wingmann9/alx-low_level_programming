ae5ce4b5c0a939201a4fbc8b2d46bcc47edbd5f1
wingmann <ema_eno@zoho.com>
2022-11-17 01:37:14 +0000

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: Name to be printed.
+ * @f: Pointer to a function that prints a name.
+ *
+ * Return: Void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		f(name);	
+}
