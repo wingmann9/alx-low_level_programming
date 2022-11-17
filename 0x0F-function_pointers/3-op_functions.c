3771d76be6517fe34ddeca4b65110cbdca5ee5d7
wingmann <ema_eno@zoho.com>
2022-11-17 01:56:55 +0000

    Operator functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Summation of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the subtraction between two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the multiplication of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Modulo of a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
