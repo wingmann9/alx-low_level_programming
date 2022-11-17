7f13e7da1c2b78dd5445a47983fec904ac32984e
wingmann <ema_eno@zoho.com>
2022-11-17 02:26:59 +0000

    Fix error

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,8 +4,10 @@
 #include <stddef.h>
 
 /**
- * File: function_pointer.h
- * Auth: Ema-Eno Simon.
+ * File - function_pointers.h
+ * @name: Nmae to be printed.
+ * @f: Pointer to a function that prints a name.
+ *
  * Desc: Header file containing the functions prototype in the directory.
  */
 
@@ -13,4 +15,4 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /* FUNCTION_POINTERS_H */
