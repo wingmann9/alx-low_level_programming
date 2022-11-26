60fa88a6d8ae35ef17d6b5c6b0d92d71295ac9e4
wingmann <ema_eno@zoho.com>
2022-11-26 20:26:42 +0000

    Free the list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the list_t list.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
