490ade178b913cf402eb3ba551b79a64b4f96735
wingmann <ema_eno@zoho.com>
2022-11-26 20:28:30 +0000

    64-bit program that print a string

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+; Desc: 64-bits assembly program thats prints a string.
+
+extern printf
+
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
