19c01cd728f5df2a72cab2caffbe8896037337cb
wingmann <ema_eno@zoho.com>
2022-12-01 01:35:49 +0000

    Find the loop in a linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the loop contained in
+ *                     a listint_t linked list.
+ * @head: Pointer to the head of the listint_t list.
+ *
+ * Return: Address of the node where the loop starts, otherwise NULL.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (NULL);
+}
