1f4490438464f735d21c4152b4f250ca478e621d
wingmann <ema_eno@zoho.com>
2022-12-01 01:21:15 +0000

    Add new node to the beginning of a llinked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds a new node at the beginning
+ *               of a listint_t list.
+ * @head: Pointer to the address of the
+ *        head of the listint_t list.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: Address of the new element, otherwise NULL.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+	return (new);
+}
